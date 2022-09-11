6af78de7233fbd64d6a85fa0b727a815d9b105c5
Albi Kozeli <albikoze@buffalo.edu>
2022-09-10 22:27:29 -0400

    update

--- app/src/main/java/com/example/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/guesstheword/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.guesstheword
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.color
 import com.example.wordguess.FourLetterWordList
-import com.github.jinatonic.confetti.CommonConfetti
+import com.github.jinatonic.confetti.CommonConfetti;
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         val simpleEditText =  findViewById<EditText>(R.id.WordEnter)
         val button = findViewById<Button>(R.id.button)
-        val guessTexts = arrayListOf<TextView>(findViewById<TextView>(R.id.Guess1),findViewById<TextView>(R.id.Guess2),
-            findViewById<TextView>(R.id.Guess3))
-        val checkTexts = arrayListOf<TextView>(findViewById<TextView>(R.id.Check1), findViewById<TextView>(R.id.Check2),
-            findViewById<TextView>(R.id.Check3))
+        val guessTexts = arrayListOf<TextView>(findViewById(R.id.Guess1),findViewById(R.id.Guess2),
+            findViewById(R.id.Guess3))
+        val checkTexts = arrayListOf<TextView>(findViewById(R.id.Check1), findViewById(R.id.Check2),
+            findViewById(R.id.Check3))
         val answer = findViewById<TextView>(R.id.textView)
 
 
@@ -49,13 +56,15 @@ class MainActivity : AppCompatActivity() {
             }
             else{
 
+                it.hideKeyboard()
                 guessTexts[pos].text = strValue
-                val actual = checkGuess(strValue) == "OOOO"
-                checkTexts[pos].text = checkGuess(strValue)
+                val actual = checkGuess(strValue)
+                checkTexts[pos].text = actual.first
                 pos++
 
 
-                if (actual or (pos==3)){
+
+                if (actual.third or (pos==3)){
 
                     button.text = "RESET"
                     answer.text = "Answer:" + wordToGuess
@@ -77,19 +86,26 @@ class MainActivity : AppCompatActivity() {
      *   '+' represents the right letter in the wrong place
      *   'X' represents a letter not in the target word
      */
-    private fun checkGuess(guess: String) : String {
-        var result = ""
+    private fun checkGuess(guess: String) : Triple<SpannableStringBuilder,Int,Boolean> {
+        var result = SpannableStringBuilder()
+        var correct = 0
         for (i in 0..3) {
             if (guess[i] == wordToGuess[i]) {
-                result += "O"
+                result.color(Color.GREEN, {append(guess[i])})
+                correct++
             }
             else if (guess[i] in wordToGuess) {
-                result += "+"
+                result.color(Color.YELLOW, {append(guess[i])})
             }
             else {
-                result += "X"
+                result.append(guess[i].toString())
             }
         }
-        return result
+        return Triple(result, correct, correct==4)
+    }
+
+    fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
